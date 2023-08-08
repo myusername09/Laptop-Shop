82eb026ceb0c0c676c1d8f6076b2d3a18bafa475
Komal Maruti Landge <106883996+myusername09@users.noreply.github.com>
2023-08-08 20:05:07 +0530

    Create Neworder.java
    Model

---
+++ Neworder.java
@@ -0,0 +1,70 @@
+package com.example.demo.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Table(name="OrderItems")
+@Data
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Neworder {
+	@Id
+	private String firstname;
+	private String lastname;
+	private String address1;
+	    private String address2;
+	    private String country;
+    private long number;
+    private String state;
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getAddress1() {
+		return address1;
+	}
+	public void setAddress1(String address1) {
+		this.address1 = address1;
+	}
+	public String getAddress2() {
+		return address2;
+	}
+	public void setAddress2(String address2) {
+		this.address2 = address2;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public long getNumber() {
+		return number;
+	}
+	public void setNumber(long number) {
+		this.number = number;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+    
+	
+}
