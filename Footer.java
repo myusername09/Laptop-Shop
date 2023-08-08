9794d3b5ed9ca7d6d3e5ac5c3f02ecdb09235b80
Komal Maruti Landge <106883996+myusername09@users.noreply.github.com>
2023-08-08 20:03:50 +0530

    Create Footer.java
    Model

---
+++ Footer.java
@@ -0,0 +1,42 @@
+package com.example.demo.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Data
+@Table(name="FeedBack")
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Footer {
+	@Id
+	private String name;
+	private String email;
+	private String comments;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getComments() {
+		return comments;
+	}
+	public void setComments(String comments) {
+		this.comments = comments;
+	}
+	
+
+}
